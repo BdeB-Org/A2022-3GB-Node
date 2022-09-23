00cbfc4f4defb6e7b8e87194acd277493055f075
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2022-09-23 09:02:53 -0400

    fichiers initiaux

---
+++ connect2.js
@@ -0,0 +1,92 @@
+const fs = require("fs");
+const oracledb = require("oracledb");
+const dbConfig = require("./dbconfig2.js");
+
+// On Windows and macOS, you can specify the directory containing the Oracle
+// Client Libraries at runtime, or before Node.js starts.  On other platforms
+// the system library search path must always be set before Node.js is started.
+// See the node-oracledb installation documentation.
+// If the search path is not correct, you will get a DPI-1047 error.
+let libPath;
+if (process.platform === "win32") {
+  // Windows
+  libPath = "C:\\oracle\\instantclient_19_12";
+} else if (process.platform === "darwin") {
+  // macOS
+  libPath = process.env.HOME + "/instantclient_19_8";
+}
+if (libPath && fs.existsSync(libPath)) {
+  oracledb.initOracleClient({ libDir: libPath });
+}
+
+async function run() {
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+
+    console.log("Successfully connected to Oracle Database");
+
+    // Create a table
+
+    await connection.execute(`begin
+                                execute immediate 'drop table todoitem';
+                                exception when others then if sqlcode <> -942 then raise; end if;
+                              end;`);
+
+    await connection.execute(`create table todoitem (
+                                id number generated always as identity,
+                                description varchar2(4000),
+                                creation_ts timestamp with time zone default current_timestamp,
+                                done number(1,0),
+                                primary key (id))`);
+
+    // Insert some data
+
+    const sql = `insert into todoitem (description, done) values(:1, :2)`;
+
+    const rows = [
+      ["Task 1", 0],
+      ["Task 2", 0],
+      ["Task 3", 1],
+      ["Task 4", 0],
+      ["Task 5", 1],
+    ];
+
+    let result = await connection.executeMany(sql, rows);
+
+    console.log(result.rowsAffected, "Rows Inserted");
+
+    connection.commit();
+
+    // Now query the rows back
+
+    result = await connection.execute(
+      `select description, done from todoitem`,
+      [],
+      { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT }
+    );
+
+    const rs = result.resultSet;
+    let row;
+
+    while ((row = await rs.getRow())) {
+      if (row.DONE) console.log(row.DESCRIPTION, "is done");
+      else console.log(row.DESCRIPTION, "is NOT done");
+    }
+
+    await rs.close();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
+
+run();
