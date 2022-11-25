60c8cfce45bca9949bb6e8d0a6e1321c7eca9f14
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2022-11-25 14:01:44 -0500

    Update connect2.js

--- connect2.js
+++ connect2.js
@@ -10,7 +10,7 @@ const dbConfig = require("./dbconfig2.js");
 let libPath;
 if (process.platform === "win32") {
   // Windows
-  libPath = "C:\\oracle\\instantclient_19_16";
+  libPath = "C:\\oracle\\instantclient_19_17";
 } else if (process.platform === "darwin") {
   // macOS
   libPath = process.env.HOME + "/instantclient_19_8";
